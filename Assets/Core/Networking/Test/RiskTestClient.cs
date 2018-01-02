0eed09161a95e61ad8ac098c1044acabb386f561
Errorcodebin <errorcodebin_2@hotmail.com>
2018-01-02 17:43:12 +0100

    Added Server Client test classes

---
+++ Assets/Core/Networking/Test/RiskTestClient.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiskTestClient : ClientBase
+{
+    void Start()
+    {
+        Connect(5010,"127.0.0.1",1000);
+    }
+
+    public void Update()
+    {
+        if (IsRunning)
+            HandleConnections();
+    }
+
+    protected override void OnDataRecieved(int clientID, byte[] buffer, int size)
+    {
+        string message = System.Text.Encoding.Unicode.GetString(buffer, 0, size);
+        Debug.Log(string.Format("client {0}: {1}", clientID, message));
+    }
+}
+
---
+++ Assets/Core/Networking/Test/RiskTestServer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class RiskTestServer : ServerBase
+{
+    void Start()
+    {
+        Startup(5010, 1000);
+    }
+
+    public void Update()
+    {
+        if (IsRunning)
+            HandleConnections();
+    }
+
+    protected override void OnConnection(int connectionID)
+    {
+        Debug.Log(string.Format("client {0}: has connected.",connectionID));
+    }
+
+    protected override void Ondisconnect(int connectionID)
+    {
+        Debug.Log(string.Format("client {0}:  has disconnected.",connectionID));
+    }
+
+    protected override void OnDataRecieved(int clientID, byte[] buffer, int size)
+    {
+        string message = System.Text.Encoding.Unicode.GetString(buffer, 0, size);
+        Debug.Log(string.Format("client {0}: {1}", clientID, message));
+    }
+}
