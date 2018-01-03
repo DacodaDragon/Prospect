d7c9ee6cf28fb570eb48a66aa00a4cd651b6549f
Errorcodebin <errorcodebin_2@hotmail.com>
2018-01-03 18:45:52 +0100

    Moved server and client classes in namespaces

--- Assets/Core/Networking/ServerClientBase/ClientBase.cs
+++ Assets/Core/Networking/ServerClientBase/ClientBase.cs
@@ -1,116 +1,119 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 
-/// <summary>
-/// Basic Unet Server with Reliable and Unreliable channel
-/// </summary>
-public abstract class ClientBase : MonoBehaviour
+namespace Game.Networking.Client
 {
-    private int m_hostID;
-    private int m_connectionID;
-    private int m_channelReliable;
-    private int m_channelUnreliable;
-
-    private byte m_error;
-
-    private bool m_running;
-    public bool IsRunning { get { return m_running; } }
-
     /// <summary>
-    /// Starts the server
+    /// Basic Unet Server with Reliable and Unreliable channel
     /// </summary>
-    public void Connect(int port, string ip, int MaxConnections)
+    public abstract class ClientBase : MonoBehaviour
     {
-        // Initialize NetworkTransport (Unet)
-        NetworkTransport.Init();
+        private int m_hostID;
+        private int m_connectionID;
+        private int m_channelReliable;
+        private int m_channelUnreliable;
 
-        ConnectionConfig config = new ConnectionConfig();
-        m_channelUnreliable = config.AddChannel(QosType.Unreliable);
-        m_channelReliable = config.AddChannel(QosType.Reliable);
+        private byte m_error;
 
-        HostTopology topology = new HostTopology(config, MaxConnections);
-        m_hostID = NetworkTransport.AddHost(topology, 0);
+        private bool m_running;
+        public bool IsRunning { get { return m_running; } }
 
-        m_connectionID = NetworkTransport.Connect(
-            m_hostID, ip, port, 0, out m_error);
-
-        m_running = true;
-    }
-
-    /// <summary>
-    /// Handles connecting, disconnecting and data send by clients.
-    /// </summary>
-    public void HandleConnections()
-    {
-        int hostID;
-        int connectionID;
-        int channelId;
-        byte[] recievedBuffer = new byte[1024];
-        int bufferSize = 1024;
-        int dataSize;
-        byte error;
-
-        NetworkEventType recievedData = NetworkTransport.Receive
-        (
-            out hostID,
-            out connectionID,
-            out channelId,
-            recievedBuffer,
-            bufferSize,
-            out dataSize,
-            out error
-        );
-
-        switch (recievedData)
+        /// <summary>
+        /// Starts the server
+        /// </summary>
+        public void Connect(int port, string ip, int MaxConnections)
         {
-            case NetworkEventType.DataEvent:
-                HandleRecievedData(connectionID, recievedBuffer, dataSize);
-                break;
+            // Initialize NetworkTransport (Unet)
+            NetworkTransport.Init();
+
+            ConnectionConfig config = new ConnectionConfig();
+            m_channelUnreliable = config.AddChannel(QosType.Unreliable);
+            m_channelReliable = config.AddChannel(QosType.Reliable);
+
+            HostTopology topology = new HostTopology(config, MaxConnections);
+            m_hostID = NetworkTransport.AddHost(topology, 0);
+
+            m_connectionID = NetworkTransport.Connect(
+                m_hostID, ip, port, 0, out m_error);
+
+            m_running = true;
         }
-    }
 
-    /// <summary>
-    /// Handles Incoming Data
-    /// </summary>
-    /// <param name="connectionid">id of the connection</param>
-    /// <param name="buffer">buffer of bytes containing data</param>
-    /// <param name="bufferSize">size of buffer</param>
-    private void HandleRecievedData(int connectionid, byte[] buffer, int bufferSize)
-    {
-        OnDataRecieved(connectionid, buffer, bufferSize);
-    }
+        /// <summary>
+        /// Handles connecting, disconnecting and data send by clients.
+        /// </summary>
+        public void HandleConnections()
+        {
+            int hostID;
+            int connectionID;
+            int channelId;
+            byte[] recievedBuffer = new byte[1024];
+            int bufferSize = 1024;
+            int dataSize;
+            byte error;
 
-    protected abstract void OnDataRecieved(int clientID, byte[] buffer, int size);
-
-    public void SendReliable(string message)
-    {
-        byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
-        SendReliable(data);
-    }
-    public void SendReliable(byte[] message)
-    {
-        NetworkTransport.Send(
-            m_hostID,
-            m_connectionID,
-            m_channelReliable,
-            message,
-            message.Length * sizeof(byte),
-            out m_error
+            NetworkEventType recievedData = NetworkTransport.Receive
+            (
+                out hostID,
+                out connectionID,
+                out channelId,
+                recievedBuffer,
+                bufferSize,
+                out dataSize,
+                out error
             );
-    }
-    public void SendUnreliable(string message)
-    {
-        byte[] data = System.Text.Encoding.Unicode.GetBytes(message);
-        SendReliable(data);
-    }
-    public void SendUnreliable(byte[] message)
-    {
-        NetworkTransport.Send(
-            m_hostID, m_connectionID,
-            m_channelUnreliable,
-            message,
-            message.Length * sizeof(byte),
-            out m_error
-            );
+
+            switch (recievedData)
+            {
+                case NetworkEventType.DataEvent:
+                    HandleRecievedData(connectionID, recievedBuffer, dataSize);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Handles Incoming Data
+        /// </summary>
+        /// <param name="connectionid">id of the connection</param>
+        /// <param name="buffer">buffer of bytes containing data</param>
+        /// <param name="bufferSize">size of buffer</param>
+        private void HandleRecievedData(int connectionid, byte[] buffer, int bufferSize)
+        {
+            OnDataRecieved(connectionid, buffer, bufferSize);
+        }
+
+        protected abstract void OnDataRecieved(int clientID, byte[] buffer, int size);
+
+        public void SendReliable(string message)
+        {
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
+            SendReliable(data);
+        }
+        public void SendReliable(byte[] message)
+        {
+            NetworkTransport.Send(
+                m_hostID,
+                m_connectionID,
+                m_channelReliable,
+                message,
+                message.Length * sizeof(byte),
+                out m_error
+                );
+        }
+        public void SendUnreliable(string message)
+        {
+            byte[] data = System.Text.Encoding.Unicode.GetBytes(message);
+            SendReliable(data);
+        }
+        public void SendUnreliable(byte[] message)
+        {
+            NetworkTransport.Send(
+                m_hostID, m_connectionID,
+                m_channelUnreliable,
+                message,
+                message.Length * sizeof(byte),
+                out m_error
+                );
+        }
     }
 }
--- Assets/Core/Networking/ServerClientBase/ServerBase.cs
+++ Assets/Core/Networking/ServerClientBase/ServerBase.cs
@@ -2,157 +2,160 @@
 using UnityEngine;
 using UnityEngine.Networking;
 
-/// <summary>
-/// Basic Unet Server with Reliable and Unreliable channel
-/// </summary>
-public abstract class ServerBase : MonoBehaviour
+namespace Game.Networking.Server
 {
-
-    private List<int> m_connections;
-    private int m_hostID;
-    private int m_channelReliable;
-    private int m_ChannelUnreliable;
-    private byte m_error;
-
-    private bool m_running;
-    public bool IsRunning { get { return m_running; } }
-
     /// <summary>
-    /// Starts the server
+    /// Basic Unet Server with Reliable and Unreliable channel
     /// </summary>
-    /// <param name="port">port to host the server on</param>
-    /// <param name="MaxConnections">maximum connections allowed</param>
-    public void Startup(int port, int MaxConnections)
+    public abstract class ServerBase : MonoBehaviour
     {
-        // Initialize NetworkTransport (Unet)
-        NetworkTransport.Init();
 
-        ConnectionConfig config = new ConnectionConfig();
-        m_ChannelUnreliable = config.AddChannel(QosType.Unreliable);
-        m_channelReliable   = config.AddChannel(QosType.Reliable);
+        private List<int> m_connections;
+        private int m_hostID;
+        private int m_channelReliable;
+        private int m_ChannelUnreliable;
+        private byte m_error;
 
-        HostTopology topology = new HostTopology(config, MaxConnections);
-        m_hostID = NetworkTransport.AddHost(topology, port);
+        private bool m_running;
+        public bool IsRunning { get { return m_running; } }
 
-        m_connections = new List<int>();
-        m_running = true;
-    }
-
-    /// <summary>
-    /// Handles connecting, disconnecting and data send by clients.
-    /// </summary>
-    public void HandleConnections()
-    {
-        int hostID;
-        int connectionID;
-        int channelId;
-        byte[] recievedBuffer = new byte[1024];
-        int bufferSize = 1024;
-        int dataSize;
-        byte error;
-
-        NetworkEventType recievedData = NetworkTransport.Receive
-        (
-            out hostID,
-            out connectionID,
-            out channelId,
-            recievedBuffer,
-            bufferSize,
-            out dataSize,
-            out error
-        );
-
-        switch (recievedData)
+        /// <summary>
+        /// Starts the server
+        /// </summary>
+        /// <param name="port">port to host the server on</param>
+        /// <param name="MaxConnections">maximum connections allowed</param>
+        public void Startup(int port, int MaxConnections)
         {
-            case NetworkEventType.ConnectEvent:
-                HandleConnect(connectionID);
-                break;
-            case NetworkEventType.DisconnectEvent:
-                HandleDisconnect(connectionID);
-                break;
-            case NetworkEventType.DataEvent:
-                HandleRecievedData(connectionID, recievedBuffer, dataSize);
-                break;
+            // Initialize NetworkTransport (Unet)
+            NetworkTransport.Init();
+
+            ConnectionConfig config = new ConnectionConfig();
+            m_ChannelUnreliable = config.AddChannel(QosType.Unreliable);
+            m_channelReliable = config.AddChannel(QosType.Reliable);
+
+            HostTopology topology = new HostTopology(config, MaxConnections);
+            m_hostID = NetworkTransport.AddHost(topology, port);
+
+            m_connections = new List<int>();
+            m_running = true;
         }
-    }
 
-    /// <summary>
-    /// Handles incoming connection requests
-    /// </summary>
-    /// <param name="connectionid">id of the connection</param>
-    private void HandleConnect(int connectionid)
-    {
-        m_connections.Add(connectionid);
-        OnConnection(connectionid);
-    }
-
-    /// <summary>
-    /// Handles incoming disconnection requests
-    /// </summary>
-    /// <param name="connectionid">id of the connection</param>
-    private void HandleDisconnect(int connectionid)
-    {
-        m_connections.Remove(connectionid);
-        Ondisconnect(connectionid);
-    }
-
-    /// <summary>
-    /// Handles Incoming Data
-    /// </summary>
-    /// <param name="connectionid">id of the connection</param>
-    /// <param name="buffer">buffer of bytes containing data</param>
-    /// <param name="bufferSize">size of buffer</param>
-    private void HandleRecievedData(int connectionid, byte[] buffer, int bufferSize)
-    {
-        OnDataRecieved(connectionid, buffer, bufferSize);
-    }
-
-    protected abstract void OnConnection(int connectionID);
-    protected abstract void Ondisconnect(int connectionID);
-    protected abstract void OnDataRecieved(int clientID, byte[] buffer, int size);
-
-    public void SendReliable(string message)
-    {
-        SendReliable(message, m_connections.ToArray());
-    }
-    public void SendReliable(string message, params int[] client)
-    {
-        byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
-        SendReliable(data, client);
-    }
-    public void SendReliable(byte[] message, params int[] client)
-    {
-        for (int i = 0; i < client.Length; i++)
+        /// <summary>
+        /// Handles connecting, disconnecting and data send by clients.
+        /// </summary>
+        public void HandleConnections()
         {
-            NetworkTransport.Send
-                (
-                m_hostID, client[i],
-                m_channelReliable,
-                message,
-                message.Length * sizeof(byte),
-                out m_error);
+            int hostID;
+            int connectionID;
+            int channelId;
+            byte[] recievedBuffer = new byte[1024];
+            int bufferSize = 1024;
+            int dataSize;
+            byte error;
+
+            NetworkEventType recievedData = NetworkTransport.Receive
+            (
+                out hostID,
+                out connectionID,
+                out channelId,
+                recievedBuffer,
+                bufferSize,
+                out dataSize,
+                out error
+            );
+
+            switch (recievedData)
+            {
+                case NetworkEventType.ConnectEvent:
+                    HandleConnect(connectionID);
+                    break;
+                case NetworkEventType.DisconnectEvent:
+                    HandleDisconnect(connectionID);
+                    break;
+                case NetworkEventType.DataEvent:
+                    HandleRecievedData(connectionID, recievedBuffer, dataSize);
+                    break;
+            }
         }
-    }
-    public void SendUnreliable(string message)
-    {
-        SendUnreliable(message, m_connections.ToArray());
-    }
-    public void SendUnreliable(string message, params int[] client)
-    {
-        byte[] data = System.Text.Encoding.Unicode.GetBytes(message);
-        SendReliable(data, client);
-    }
-    public void SendUnreliable(byte[] message, params int[] client)
-    {
-        for (int i = 0; i < client.Length; i++)
+
+        /// <summary>
+        /// Handles incoming connection requests
+        /// </summary>
+        /// <param name="connectionid">id of the connection</param>
+        private void HandleConnect(int connectionid)
         {
-            NetworkTransport.Send
-                (
-                m_hostID, client[i],
-                m_ChannelUnreliable,
-                message,
-                message.Length * sizeof(byte),
-                out m_error);
+            m_connections.Add(connectionid);
+            OnConnection(connectionid);
+        }
+
+        /// <summary>
+        /// Handles incoming disconnection requests
+        /// </summary>
+        /// <param name="connectionid">id of the connection</param>
+        private void HandleDisconnect(int connectionid)
+        {
+            m_connections.Remove(connectionid);
+            Ondisconnect(connectionid);
+        }
+
+        /// <summary>
+        /// Handles Incoming Data
+        /// </summary>
+        /// <param name="connectionid">id of the connection</param>
+        /// <param name="buffer">buffer of bytes containing data</param>
+        /// <param name="bufferSize">size of buffer</param>
+        private void HandleRecievedData(int connectionid, byte[] buffer, int bufferSize)
+        {
+            OnDataRecieved(connectionid, buffer, bufferSize);
+        }
+
+        protected abstract void OnConnection(int connectionID);
+        protected abstract void Ondisconnect(int connectionID);
+        protected abstract void OnDataRecieved(int clientID, byte[] buffer, int size);
+
+        public void SendReliable(string message)
+        {
+            SendReliable(message, m_connections.ToArray());
+        }
+        public void SendReliable(string message, params int[] client)
+        {
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
+            SendReliable(data, client);
+        }
+        public void SendReliable(byte[] message, params int[] client)
+        {
+            for (int i = 0; i < client.Length; i++)
+            {
+                NetworkTransport.Send
+                    (
+                    m_hostID, client[i],
+                    m_channelReliable,
+                    message,
+                    message.Length * sizeof(byte),
+                    out m_error);
+            }
+        }
+        public void SendUnreliable(string message)
+        {
+            SendUnreliable(message, m_connections.ToArray());
+        }
+        public void SendUnreliable(string message, params int[] client)
+        {
+            byte[] data = System.Text.Encoding.Unicode.GetBytes(message);
+            SendReliable(data, client);
+        }
+        public void SendUnreliable(byte[] message, params int[] client)
+        {
+            for (int i = 0; i < client.Length; i++)
+            {
+                NetworkTransport.Send
+                    (
+                    m_hostID, client[i],
+                    m_ChannelUnreliable,
+                    message,
+                    message.Length * sizeof(byte),
+                    out m_error);
+            }
         }
     }
 }
